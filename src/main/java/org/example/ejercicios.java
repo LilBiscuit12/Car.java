af3dfae4eb786757e0f1d6d920c4964a4d634305
Stefano Micciche <miccichestefano20@gmail.com>
2024-10-29 20:19:38 +0100

    Realizamos ejercicio circuferencia (Falta correcion)

--- src/main/java/org/example/ejercicios.java
+++ src/main/java/org/example/ejercicios.java
@@ -7,12 +7,17 @@ public class ejercicios {
 //por pantalla la longitud y el área de la circunferencia.
 //Longitud de la circunferencia = 2*PI*Radio, Area de la circunferencia = PI*Radio^2
 
+    // Falta corregir
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double Circunferencia, Longitud;
+        double radio, Circunferencia, Longitud;
         System.out.printf("Imprime la Circuferencia y Longitud");
         sc.nextLine();
-
+        radio = sc.nextDouble();
+        Circunferencia = 2* Math.PI * radio;
+        Longitud = 2* Math.PI * radio;
+        System.out.println("Longitud de la circunferencia " + radio);
+        System.out.println("Longitud de la circunferencia " + radio);
     }
 
 
